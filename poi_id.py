4e1967d6ee037b83652b766d1655d22e081fd178
huangli <ekinasm@gmail.com>
2017-02-12 08:59:44 +0800

    add pipeline

--- poi_id.py
+++ poi_id.py
@@ -2,6 +2,7 @@
 
 import sys
 import pickle
+import matplotlib.pyplot as plt
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -10,7 +11,12 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+# You will need to use more features
+features_list = ['poi','salary', 'deferral_payments', 'total_payments', 'loan_advances',
+ 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value',
+ 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive',
+ 'restricted_stock', 'director_fees','to_messages', 'from_poi_to_this_person',
+  'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -21,9 +27,18 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
+
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+# for point in data:
+#     salary = point[1]
+#     total_payments = point[2]
+#     plt.hist(total_payments, bins='auto')
+
+# plt.show()
+# print data[1,:]
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -32,20 +47,40 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn.pipeline import Pipeline
+# from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA
+# from sklearn.feature_selection import SelectKBest
+# from sklearn.linear_model import LogisticRegression
+# clf = GaussianNB()
+# clf = SVC(kernel='linear', C=2)
+# clf = Pipeline([('reduce_dim', PCA(n_components=4)), ('clf', GaussianNB())])
+clf = Pipeline([('reduce_dim', PCA(n_components=4)), ('clf', SVC(kernel='poly'))])
+# clf = Pipeline([('reduce_dim', PCA(n_components=4)), ('clf', LogisticRegression())])
+# clf = Pipeline(estimators)
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+print clf
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 from sklearn.cross_validation import train_test_split
+
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+# print features_train[0:1]
+# selector = SelectKBest(f_classif, k=5).fit(features_train, labels_train)
+# print selector.get_support()
+# print selector.scores_
+
+# clf.fit(features_train, labels_train)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
