091e8f341ee3443c62d759ff88bc0e860e9a7035
huangli <ekinasm@gmail.com>
2017-02-16 16:42:40 +0800

    test whether email to poi again

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Udacity-Project5
+
+### Goal of this project
+
+Enron was one of the largest companies in the United States,
+By 2002, it had collapsed into bankruptcy due to widespread corporate fraud.
+A significant amount of typically confidential information entered into the public record,
+we are trying to building a person of interest identifier algorithm,
+based on financial and email data made public as a result of the Enron scandal.
+
+### Features and scaling
+
+#### features
+
+I create a function hist_and_save_pic which is now commented im poi_id.py, it will print and save
+the feature histogram in png file. After explore all the features, I remove loan_advances,
+restricted_stock_deferred, deferred_income, director_fees. Regarding the email features, I test
+the correrlation between 'from_poi_to_this_person','from_this_person_to_poi' and 'shared_receipt_with_poi'
+it's all highly related, so I only keep one feature from email features: 'shared_receipt_with_poi'.
+I begin my test with decision tree classifier, as it turns out, only 'total_payments','restricted_stock_deferred', 'expenses'
+are corrleated to precision and recall.
+
+
+#### scaling
+
+I didn't do any scaling, the scaling is not helpful for decision tree algorithm.
+
+### new feature
+
--- poi_id.py
+++ poi_id.py
@@ -65,33 +65,32 @@ from tester import dump_classifier_and_data
 # hist_and_save_pic((data[:,19]), "shared_receipt_with_poi")
 # print sum(data[:,19] == 0)
 
-
-features_list = ['poi', 'total_payments', 'loan_advances',
- 'restricted_stock_deferred', 'expenses', 'long_term_incentive', 'from_this_person_to_poi']
+features_list = ['poi', 'total_payments','restricted_stock_deferred', 'expenses', 'shared_receipt_with_poi']
  # add whether_email_to_poi
-my_features_list = ['poi', 'total_payments', 'loan_advances',
- 'restricted_stock_deferred', 'expenses', 'long_term_incentive', 'whether_email_to_poi']
+# my_features_list = ['poi', 'total_payments', 'loan_advances', 'restricted_stock_deferred', 'expenses', 'whether_email_to_poi']
 
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
+my_dataset = data_dict
 
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
-my_dataset = data_dict
-for k1 in my_dataset:
-    if my_dataset[k1]['from_this_person_to_poi'] > 0:
-        my_dataset[k1]['whether_email_to_poi'] = 1
-    else:
-        my_dataset[k1]['whether_email_to_poi'] = 0
-    # k1['whether_email_to_poi']
 
-print my_dataset
+# new feature seems not working well
+# for k1 in my_dataset:
+#     if my_dataset[k1]['from_this_person_to_poi'] == 'NaN':
+#         my_dataset[k1]['whether_email_to_poi'] = 0
+#     else:
+#         my_dataset[k1]['whether_email_to_poi'] = 1
+
+# print my_dataset
 ### Extract features and labels from dataset for local testing
-data = featureFormat(my_dataset, features_list, sort_keys = True)
+data = featureFormat(my_dataset, my_features_list, sort_keys = True)
 
+# print np.corrcoef(data[:,4],data[:,6])
 
 
 
@@ -103,16 +102,15 @@ def outlier_detect(data):
     outlier_idx = np.where(outlier_indicate==True)
     return outlier_idx
 
-# idx = outlier_detect(data[:,1])
+idx = outlier_detect(data[:,1])
 from sets import Set
 outlier_idx = Set()
-for i in range(1,6):
+for i in range(1,len(my_features_list)-1):
     idx = outlier_detect(data[:,i])
-    print idx
     for ii in idx[0]:
         outlier_idx.add(ii)
 
-print outlier_idx
+# print outlier_idx
 data = np.delete(data, list(outlier_idx), 0)
 labels, features = targetFeatureSplit(data)
 
@@ -125,13 +123,13 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.pipeline import Pipeline
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
-from sklearn.decomposition import PCA
+# from sklearn.pipeline import Pipeline
+# from sklearn.naive_bayes import GaussianNB
+# from sklearn.svm import SVC
+# from sklearn.decomposition import PCA
 from sklearn import tree
-from sklearn.feature_selection import SelectKBest, f_classif
-from sklearn.model_selection import GridSearchCV
+# from sklearn.feature_selection import SelectKBest, f_classif
+# from sklearn.model_selection import GridSearchCV
 # from sklearn.linear_model import LogisticRegression
 # clf = GaussianNB()
 # clf = SVC(kernel='linear', C=2)
@@ -156,8 +154,8 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
-pca = PCA(n_components = 4)
-pca.fit(features_train)
+# pca = PCA(n_components = 2)
+# pca.fit(features_train)
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(features_train, labels_train)
@@ -176,9 +174,9 @@ print "recall", recall_score(pred, labels_test)
 # print selector.scores_
 # clf.fit(features_train, labels_train)
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
+### Task 6: Dump your classifier, dataset, and my_features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, my_features_list)
