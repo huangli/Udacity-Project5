fix: precision and recall parameter tuning, reselect 4 feature for tuning

poi_id.py
@@ -16,18 +16,17 @@ from tester import dump_classifier_and_data
 ### The first feature must be "poi".
 # You will need to use more features
 
-# features_list = ['poi', 'to_messages', 'from_poi_to_this_person',
+# features_list = ['poi', 'to_messages', 'from_this_person_to_poi',
 # 'total_payments','director_fees', 'deferral_payments',  'loan_advances', 'salary',
 # 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
 # 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock',
-#  'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
-features_list = ['poi', 'to_messages', 'from_poi_to_this_person', 'total_payments','director_fees']
+#  'from_messages', 'from_poi_to_this_person', 'shared_receipt_with_poi']
+features_list = ['poi', 'salary', 'loan_advances', 'deferred_income','expenses']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 my_dataset = data_dict
-# print len(my_dataset)
 
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
@@ -45,7 +44,7 @@ for k in to_delete_idx:
     del(my_dataset[k])
 
 df = pd.DataFrame.from_dict(data_dict, orient='index', dtype=np.float)
-print df['total_payments'].idxmax()
+# print df['total_payments'].idxmax()
 max_outlier = df['total_payments'].argmax()
 del(my_dataset[max_outlier])
 
@@ -53,12 +52,7 @@ del(my_dataset[max_outlier])
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 print len(data)
 
-### remove outlier
 labels, features = targetFeatureSplit(data)
-
-# 4 features picked,to_messages,from_this_person_to_poi,total_payments,director_fees
-
-
 
 ### create new feature, add whether_shared_receipt_with_poi
 
@@ -71,7 +65,6 @@ labels, features = targetFeatureSplit(data)
 # Provided to give you a starting point. Try a variety of classifiers.
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
-from sklearn.decomposition import PCA
 from sklearn import tree
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.model_selection import GridSearchCV
@@ -94,41 +87,38 @@ features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.2, random_state=42)
 
 ### Set the parameters by cross-validation
-### SVM
+## SVM
+# recall 1.0, C:1e-5, gamma:0, weight:true:6
+# precision:0.66, C:100, gamma:0.2, weight:2
 tuned_parameters = {
-                       'clf__C': [1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 1e2, 1e3, 1e4, 1e5],
-                       'clf__gamma': [0.0, 0.1, 0.2, 0.3, 0.4, 0.5],
+                       'clf__C': [0.5, 0.75, 1.5],
+                       'clf__gamma': [0.0, 0.1, 0.2],
                        'clf__kernel': ['rbf'],
                        'clf__tol': [1e-1, 1e-2, 1e-4, 1e-5],
-                       'clf__class_weight': [{True: 12, False: 1},
-                                               {True: 10, False: 1},
-                                               {True: 9, False: 1},
-                                               {True: 8, False: 1},
-                                               {True: 7, False: 1},
-                                               {True: 4, False: 1},
-                                               {True: 1, False: 1},
-                                                ]
+                       'clf__class_weight': ['auto']
                       }
 
 pipe = Pipeline([('min/max scaler', MinMaxScaler(feature_range=(0.0, 1.0))),
                 ('clf', SVC())])
-cv = StratifiedShuffleSplit(labels, n_iter = 10, test_size=0.2, random_state = 42)
-a_grid_search = GridSearchCV(pipe, param_grid=tuned_parameters, cv=cv, scoring='precision')
+# scoring_parameters = 'precision,recall'
+cv = StratifiedShuffleSplit(labels, n_iter = 20, test_size=0.2, random_state = 42)
+a_grid_search = GridSearchCV(pipe, param_grid=tuned_parameters, scoring='precision', cv=cv,  n_jobs=8)
 a_grid_search.fit(features, labels)
 clf = a_grid_search.best_estimator_
 
-# ## Decision Tree
+## Decision Tree
 # tuned_parameters = {'clf__max_depth': [2, 3, 4, 5, 6, 7, 8, 9, 10],
 #                      'clf__min_samples_split': [2, 3, 4, 5, 6, 7, 8, 9, 10],
 #                      'clf__min_samples_leaf': [2, 3, 4, 5, 6, 7, 8, 9, 10]
 #                      }
 # pipe = Pipeline([('clf', tree.DecisionTreeClassifier())])
-# cv = StratifiedShuffleSplit(labels, n_iter = 10, test_size=0.2, random_state = 42)
-# a_grid_search = GridSearchCV(pipe, param_grid=tuned_parameters, cv=cv, scoring='precision')
+# cv = StratifiedShuffleSplit(labels, n_iter = 50, test_size=0.2, random_state = 42)
+# a_grid_search = GridSearchCV(pipe, param_grid=tuned_parameters, cv=cv, scoring='precision', n_jobs=8)
 # a_grid_search.fit(features, labels)
 # clf = a_grid_search.best_estimator_
 
-### test
+
+# ### test
 pred = clf.predict(features_test)
 
 from sklearn.metrics import accuracy_score
@@ -143,7 +133,6 @@ print "recall", recall_score(pred, labels_test)
 # selector = SelectKBest(f_classif, k=4).fit(features_train, labels_train)
 # print selector.get_support()
 # print selector.scores_
-# clf.fit(features_train, labels_train)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure